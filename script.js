cad3fb21aa2e0ab61c494a897fb84c798ccb7239
mishkatolochko <tolochko1995@yandex.ru>
2021-12-01 21:31:47 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Hello World!")
+console.log ("Hello console")
+
+const title = 10
+console.log (title);
+const screens = 30
+console.log ('screens', title);
+const screenPrice = 50
+console.log ('screenPrice');
+const rollback = 40
+console.log ('rollback', rollback);
+const fullPrice = 700
+console.log ('fullPrice', fullPrice);
+const adaptive = 300
+console.log ('adaptive', adaptive);
